e29eb017a93905b0fa9a524bda2259d7b9d4409e
LIZXP <Peter.xli@outlook.com>
2022-12-15 21:56:18 -0700

    add more notes

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -7,6 +7,7 @@ function NavBar() {
       <div className="Nav-container"></div>
     </div>
     // will be add 2 ul and 3 li for the main content
+    // will be using type writer for the type effect in here
   );
 }
 
