491456537ead7c7983ca05262b7c14cfd109d2c5
LIZXP <Peter.xli@outlook.com>
2022-12-15 21:55:41 -0700

    do the planning

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -2,9 +2,11 @@ import React from "react";
 
 function NavBar() {
   return (
+    //this div is for the main container
     <div>
       <div className="Nav-container"></div>
     </div>
+    // will be add 2 ul and 3 li for the main content
   );
 }
 
