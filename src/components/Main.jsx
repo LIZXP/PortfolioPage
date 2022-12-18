1475cc5931ef97d2b7844b777ed3a86eaf98292b
LIZXP <Peter.xli@outlook.com>
2022-12-17 23:24:18 -0700

    add contents to the main page and add pictures

---
+++ src/components/Main.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import background from "../assets/bg.svg";
+import { HiArrowNarrowRight } from "react-icons/hi";
+import mtm from "../assets/eve.svg";
+
+function Main() {
+  return (
+    <div
+      className="h-screen w-screen"
+      style={{
+        backgroundImage: "url(" + background + ")",
+        backgroundPosition: "center",
+        backgroundSize: "cover",
+        backgroundRepeat: "no-repeat",
+        height: "100vh",
+        weight: "100vw",
+      }}
+      name="home"
+    >
+      <div className="mx-auto px-4 flex flex-col items-center justify-center h-full text-black md:flex-row max-w-screen-2xl">
+        <div className="flex flex-col justify-center h-full max-w-screen-sm">
+          <h2 className="text-5xl md:text-7xl font-bold pb-5">
+            I am a Full Stack Developer
+          </h2>
+          <p className="text-black py-4 text-justify font-medium">
+            With a unique blend of technical expertise and real-world business
+            experience. My skills include proficiency in a variety of languages
+            and frameworks, as well as experience working with systems and SQL
+            and NoSQL databases. I am passionate about using my skills to
+            streamline and automate processes in apps, saving users time and
+            prioritizing focus on business functions.
+          </p>
+          <div>
+            <button>
+              Portfolio
+              <span>
+                <HiArrowNarrowRight />
+              </span>
+            </button>
+          </div>
+        </div>
+        <div>
+          <img src={mtm} alt="evolution monkey to man picture" />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Main;
