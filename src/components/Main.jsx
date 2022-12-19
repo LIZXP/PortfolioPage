954c9fbd90d39b683368131fadc15d6677c5b566
LIZXP <Peter.xli@outlook.com>
2022-12-18 22:05:51 -0700

    add icon and adjust font

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,6 +2,20 @@ import React from "react";
 import background from "../assets/bg.svg";
 import { HiArrowNarrowRight } from "react-icons/hi";
 import mtm from "../assets/eve.svg";
+import django from "../assets/django.svg";
+import css from "../assets/css.svg";
+import firebase from "../assets/firebase.svg";
+import graphql from "../assets/graphql.svg";
+import html from "../assets/html.svg";
+import javascript from "../assets/javascript.svg";
+import mongodb from "../assets/mongodb.svg";
+import next from "../assets/next.svg";
+import react from "../assets/react.svg";
+import redux from "../assets/redux.svg";
+import sass from "../assets/sass.svg";
+import tailwind from "../assets/tailwind.svg";
+import typescript from "../assets/typescript.svg";
+import python from "../assets/python.svg";
 
 function Main() {
   return (
@@ -17,30 +31,50 @@ function Main() {
       }}
       name="home"
     >
-      <div className="mx-auto px-4 flex flex-col items-center justify-center h-full text-black md:flex-row max-w-screen-2xl">
-        <div className="flex flex-col justify-center px-6 h-2/4 max-w-screen-lg md:w-2/4">
-          <h2 className="text-5xl md:text-7xl font-bold pb-5">
+      <div className="mx-auto px-4 flex flex-col items-center justify-center h-full text-black md:flex-row">
+        <div className="w-1/6 h-screen hidden items-start md:flex">
+          <div className="h-10 flex">
+            <img src={javascript} alt="" className="mr-10" />
+            <img src={css} alt="" className="mr-10" />
+            <img src={sass} alt="" className="mr-10" />
+            <img src={react} alt="" className="mr-10" />
+          </div>
+          <div className="h-10 flex">
+            <img src={typescript} alt="" className="mr-10" />
+            <img src={html} alt="" className="mr-10" />
+            <img src={python} alt="" className="mr-10" />
+            <img src={redux} alt="" className="mr-10" />
+          </div>
+        </div>
+        <div className="flex flex-col justify-center px-6 h-2/4 max-w-screen-lg md:w-1/4">
+          <h2 className="text-6xl md:text-5xl font-bold">
             I am a Full Stack Developer
           </h2>
-          <p className="text-black py-4 text-justify font-medium">
-            With a unique blend of technical expertise and real-world business
-            experience. My skills include proficiency in a variety of languages
-            and frameworks, as well as experience working with systems and SQL
-            and NoSQL databases. I am passionate about using my skills to
-            streamline and automate processes in apps, saving users time and
-            prioritizing focus on business functions.
-          </p>
           <div>
             <button>
-              Portfolio
+              Projects
               <span>
                 <HiArrowNarrowRight />
               </span>
             </button>
           </div>
         </div>
-        <div className="max-w-screen-lg px-6 md:w-2/4">
-          <img src={mtm} alt="evolution monkey to man picture" />
+        <div className="max-w-screen-lg px-6 md:w-1/4">
+          <img src={mtm} alt="evolution monkey to man" />
+        </div>
+        <div className="h-screen hidden items-start w-1/6 md:flex">
+          <div className="h-10 flex">
+            <img src={django} alt="" className="mr-10" />
+            <img src={css} alt="" className="mr-10" />
+            <img src={mongodb} alt="" className="mr-10" />
+            <img src={graphql} alt="" className="mr-10" />
+          </div>
+          <div className="h-10 flex">
+            <img src={tailwind} alt="" className="mr-10" />
+            <img src={firebase} alt="" className="mr-10" />
+            <img src={next} alt="" className="mr-10" />
+            <img src={graphql} alt="" className="mr-10" />
+          </div>
         </div>
       </div>
     </div>
