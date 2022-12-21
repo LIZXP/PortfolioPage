b3d05474cbb3b33b3ce34d0af4757a5242099c6f
LIZXP <Peter.xli@outlook.com>
2022-12-21 10:11:02 -0700

    add about content and put styles for it

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -16,47 +16,52 @@ import python from "../assets/python.svg";
 
 function About() {
   return (
-    <div name="about" className="w-full h-screen text-white bg-[#001122]">
+    <div name="about" className="w-full h-screen px-4 text-white bg-[#001122]">
+      <div className="mx-auto px-6 pt-36 flex flex-col justify-center w-full max-w-screen-2xl">
+        <div className="font-bold text-5xl pb-6 text-[#f87171]">
+          <p>About</p>
+        </div>
+        <p className="font-medium text-justify text-lg">
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus
+          cumque doloremque impedit culpa voluptatum ipsam omnis non
+          consequuntur iure quis et eos facere quam dolore, excepturi nostrum in
+          laudantium ea? Aspernatur odio, doloremque labore sit expedita tempore
+          sequi eaque impedit quibusdam dicta fugiat delectus corrupti repellat
+          perspiciatis? Aut possimus ut quo? Similique consequuntur est totam
+          dolorem unde sed sequi saepe. Ut pariatur error odit, labore, eius
+          aliquid, impedit ea laudantium perspiciatis ipsam maxime fugiat
+          commodi. Numquam consequatur placeat eum deserunt itaque
+          exercitationem maxime nam ea et nulla, quisquam officia odio. Tenetur
+          eos nam nisi obcaecati, omnis delectus sapiente laudantium modi ex
+          explicabo officia iure laboriosam corporis, rem maxime, optio ipsa
+          exercitationem ea quaerat itaque nobis temporibus non! Quae, aliquid
+          voluptatum. Consequuntur atque nostrum quidem iste deleniti labore
+          quia harum exercitationem! Voluptas perspiciatis illo in eaque totam
+          ad, voluptate quis officiis dolores, amet odit sed vero voluptatem
+          unde fuga nostrum labore.
+        </p>
+      </div>
       <div>
-        <p>About</p>
-      </div>
-      <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus
-        cumque doloremque impedit culpa voluptatum ipsam omnis non consequuntur
-        iure quis et eos facere quam dolore, excepturi nostrum in laudantium ea?
-        Aspernatur odio, doloremque labore sit expedita tempore sequi eaque
-        impedit quibusdam dicta fugiat delectus corrupti repellat perspiciatis?
-        Aut possimus ut quo? Similique consequuntur est totam dolorem unde sed
-        sequi saepe. Ut pariatur error odit, labore, eius aliquid, impedit ea
-        laudantium perspiciatis ipsam maxime fugiat commodi. Numquam consequatur
-        placeat eum deserunt itaque exercitationem maxime nam ea et nulla,
-        quisquam officia odio. Tenetur eos nam nisi obcaecati, omnis delectus
-        sapiente laudantium modi ex explicabo officia iure laboriosam corporis,
-        rem maxime, optio ipsa exercitationem ea quaerat itaque nobis temporibus
-        non! Quae, aliquid voluptatum. Consequuntur atque nostrum quidem iste
-        deleniti labore quia harum exercitationem! Voluptas perspiciatis illo in
-        eaque totam ad, voluptate quis officiis dolores, amet odit sed vero
-        voluptatem unde fuga nostrum labore.
-      </p>
-      <div className="w-10 m-auto flex">
-        <img src={javascript} alt="programming languages" />
-        <img src={css} alt="programming languages" />
-        <img src={sass} alt="programming languages" />
-        <img src={react} alt="programming languages" />
-        <img src={typescript} alt="programming languages" />
-        <img src={html} alt="programming languages" />
-        <img src={python} alt="programming languages" />
-        <img src={redux} alt="programming languages" />
-      </div>
-      <div className="w-10 m-auto flex">
-        <img src={django} alt="programming languages" />
-        <img src={css} alt="programming languages" />
-        <img src={mongodb} alt="programming languages" />
-        <img src={graphql} alt="programming languages" />
-        <img src={tailwind} alt="programming languages" />
-        <img src={firebase} alt="programming languages" />
-        <img src={next} alt="programming languages" />
-        <img src={graphql} alt="programming languages" />
+        <div className="w-10 m-auto flex">
+          <img src={javascript} alt="programming languages" />
+          <img src={css} alt="programming languages" />
+          <img src={sass} alt="programming languages" />
+          <img src={react} alt="programming languages" />
+          <img src={typescript} alt="programming languages" />
+          <img src={html} alt="programming languages" />
+          <img src={python} alt="programming languages" />
+          <img src={redux} alt="programming languages" />
+        </div>
+        <div className="w-10 m-auto flex">
+          <img src={django} alt="programming languages" />
+          <img src={css} alt="programming languages" />
+          <img src={mongodb} alt="programming languages" />
+          <img src={graphql} alt="programming languages" />
+          <img src={tailwind} alt="programming languages" />
+          <img src={firebase} alt="programming languages" />
+          <img src={next} alt="programming languages" />
+          <img src={graphql} alt="programming languages" />
+        </div>
       </div>
     </div>
   );
