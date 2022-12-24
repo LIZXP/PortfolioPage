5771ed334b11cf99a783af1eac891f3ea29ea512
LIZXP <Peter.xli@outlook.com>
2022-12-24 14:13:18 -0700

    finish up the styles with form and working on react sroll

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,10 +6,10 @@ function Contact() {
       <div className="flex flex-col p-4 justify-center max-w-screen-lg mx-auto h-full">
         <div className="pb-8">
           <p className="text-4xl font-bold inline border-b-4">Contact</p>
-          <p>Email me</p>
+          <p className="py-6">Submit the form to get in touch with me</p>
         </div>
-        <div>
-          <form action="submit" className="">
+        <div className="flex justify-center items-center">
+          <form action="submit" className="flex flex-col w-full md:w-1/2">
             <input
               type="text"
               name="name"
@@ -20,10 +20,11 @@ function Contact() {
               type="text"
               name="email"
               placeholder="enter your email"
-              className="p-2 bg-transparent border-2 rounded-md text-black focus:outline-none"
+              className="p-2 my-6 bg-transparent border-2 rounded-md text-black focus:outline-none"
             />
             <textarea
               name="message"
+              placeholder="Enter your message"
               rows={10}
               className="p-2 bg-transparent border-2 text-black focus:outline-none"
             ></textarea>
